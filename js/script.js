4d0049e6f0ac2db971bc1d5c290ec5d6fe098a9f
eesheesingh <chiro.singh9769@gmail.com>
2024-12-18 15:27:01 +0530

    Responsiveness added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Auto click functionality for "Join Now" button
-    const joinNowButton = document.querySelector('.btn-whatsapp a'); // Target the "Join Now" button's anchor tag
-    if (joinNowButton) {
-        setInterval(() => {
-            joinNowButton.click(); // Simulate a click every 5 seconds
-        }, 5000); // 5 seconds interval
-    } else {
-        console.warn("No 'Join Now' button found.");
-    }
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Auto click functionality for "Join Now" button
+//     const joinNowButton = document.querySelector('.btn-whatsapp a'); // Target the "Join Now" button's anchor tag
+//     if (joinNowButton) {
+//         setInterval(() => {
+//             joinNowButton.click(); // Simulate a click every 5 seconds
+//         }, 5000); // 5 seconds interval
+//     } else {
+//         console.warn("No 'Join Now' button found.");
+//     }
+// });
